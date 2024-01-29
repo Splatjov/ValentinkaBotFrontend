cdfc2e8624727a8a20a98ce8822d56b8b011d819
Kalexina <thesplatjov@gmail.com>
2024-01-29 13:56:40 +0100

    changed styles properties 3

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,5 @@
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
-<script>
-    window.Telegram.WebApp.expand();
-</script>
 
 <slot />
