4fa212149d406ea10a0454ea0684f681d55e693a
Kalexina <thesplatjov@gmail.com>
2024-01-29 13:35:17 +0100

    changed styles properties 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,25 @@
-<div id="header">
-    <h1>Валентинка Ботик</h1>
+<div id="block">
+    <div id="header">
+        <h1>Валентинка Ботик</h1>
+    </div>
+    <div id="header">
+        <h3>Пока что я отправил:</h3>
+    </div>
+    <div id="header">
+        <span id="big number">35</span>
+    </div>
+    <div id="header">
+        <h3>Валентинок</h3>
+    </div>
+    <div id="glupi">
+        <button onclick="location.href='/my_valentines'">
+            Мои Валентинки
+        </button>
+        <button onclick="location.href='/send_valentine'">
+            Отправить Валентинку
+        </button>
+    </div>
 </div>
-<div id="header">
-    <h3>Пока что я отправил:</h3>
-</div>
-<div id="header">
-    <span id="big number">35</span>
-</div>
-<div id="header">
-    <h3>Валентинок</h3>
-</div>
-<div id="glupi">
-    <button onclick="location.href='/my_valentines'">
-        Мои Валентинки
-    </button>
-    <button onclick="location.href='/send_valentine'">
-        Отправить Валентинку
-    </button>
-</div>
-
-
 
 <style>
     @import "styles.css";
